229ac6bfe55f80806ce3a2a4c158ec7664abce08
BrunoHPBarbosa <141725324+BrunoHPBarbosa@users.noreply.github.com>
2024-05-27 12:18:16 +0100

    presentation

---
+++ app/src/main/java/com/hacksprint/financetrack/FrameLayoutFragment.kt
@@ -0,0 +1,23 @@
+package com.hacksprint.financetrack
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+class FrameLayoutFragment(@LayoutRes private val layoutRes: Int): Fragment(
+    layoutRes) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutRes, container, false)
+    }
+
+
+
+}
